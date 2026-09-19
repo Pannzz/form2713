5e7d518146c8c8385285c08549ab0d2f653b4914
Pannzz <133737205+Pannzz@users.noreply.github.com>
2026-09-19 21:13:52 +0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,34 @@
+function updateprodi(){
+    const fakultasselect = document.getElementById('fakultas');
+    const prodiselect = document.getElementById('prodi');
+    const fakultasterpilih = fakultasselect.value;
+
+    const dataprodi  = {
+        FH: [{ value: "Hukum", text: "Ilmu Hukum" }],
+        FT: [{ value: "TM", text: "Teknik Mesin" }, 
+            { value: "TIN", text: "Teknik Industri" }, 
+            { value: "TS", text: "Teknik Sipil" }, 
+            { value: "TA", text: "Teknik Arsitektur" },],
+        FISIP: [{ value: "AN", text: "Administrasi Negara" }, 
+            { value: "ANI", text: "Administrasi Niaga" },
+            { value: "IK", text: "Ilmu Komunikasi" },],
+        FEB: [{ value: "M", text: "Manajemen" },
+            { value: "A", text: "Akuntansi" },
+            { value: "EPTM", text: "Ekonomi Pembangunan" },],
+        FTEIC: [{ value: "TE", text: "Teknik Elektro" },
+            { value: "TI", text: "Teknik Informatika" },
+            { value: "SISTEKIN", text: "Sistem dan Teknologi Informasi " },
+            { value: "AERO", text: "Aeronautika" },]
+    };
+    prodiselect.innerHTML = '<option value="" disabled selected>--Pilih Prodi--</option>';
+
+    if(fakultasterpilih && dataprodi[fakultasterpilih]) {dataprodi[fakultasterpilih].forEach(prodi=> {
+        const opsibaru = document.createElement('option');
+        opsibaru.value = prodi.value;
+        opsibaru.text = prodi.text;
+
+        prodiselect.add(opsibaru);
+        
+    });}
+
+}
